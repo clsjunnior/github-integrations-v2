cb7255f910d179d65903d3dcdb35626248fa3151
Celso Junior <clsjunnior@live.com>
2020-08-06 20:31:23 -0300

    Updates

---
+++ precache-manifest.24d7981bfdd6a0ec9dd0842aec8896dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1404dab170a7757fdf1a669723b149b9",
+    "url": "/github-integrations-v2/index.html"
+  },
+  {
+    "revision": "2cf8507c16d76981774c",
+    "url": "/github-integrations-v2/static/css/main.6ee0ba48.chunk.css"
+  },
+  {
+    "revision": "afe5049dc31b8a67246c",
+    "url": "/github-integrations-v2/static/js/2.1619a10d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-integrations-v2/static/js/2.1619a10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf8507c16d76981774c",
+    "url": "/github-integrations-v2/static/js/main.44b9c912.chunk.js"
+  },
+  {
+    "revision": "edb6509a4a03c37c6d89",
+    "url": "/github-integrations-v2/static/js/runtime-main.4f0b087c.js"
+  },
+  {
+    "revision": "7becc44f4f93ca02e2862abb9c0a88a9",
+    "url": "/github-integrations-v2/static/media/github-icon.7becc44f.svg"
+  },
+  {
+    "revision": "3d55b8fa35fd4bece965fc67e0458f6c",
+    "url": "/github-integrations-v2/static/media/logo-loading.3d55b8fa.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.944fca89eb5c07f3a751aa0891f66749.js"
+  "/github-integrations-v2/precache-manifest.24d7981bfdd6a0ec9dd0842aec8896dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-integrations-v2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

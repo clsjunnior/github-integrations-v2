83dc0e8c7ccc4154c75dcfb5d10f5c1cbc7598e8
Celso Junior <clsjunnior@live.com>
2020-08-06 20:27:46 -0300

    Updates

---
+++ precache-manifest.944fca89eb5c07f3a751aa0891f66749.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27924d5ebad4d99410d422873e7633da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf8507c16d76981774c",
+    "url": "/static/css/main.6ee0ba48.chunk.css"
+  },
+  {
+    "revision": "afe5049dc31b8a67246c",
+    "url": "/static/js/2.1619a10d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1619a10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf8507c16d76981774c",
+    "url": "/static/js/main.44b9c912.chunk.js"
+  },
+  {
+    "revision": "51847683353a8d4f9aed",
+    "url": "/static/js/runtime-main.c5d81975.js"
+  },
+  {
+    "revision": "7becc44f4f93ca02e2862abb9c0a88a9",
+    "url": "/static/media/github-icon.7becc44f.svg"
+  },
+  {
+    "revision": "3d55b8fa35fd4bece965fc67e0458f6c",
+    "url": "/static/media/logo-loading.3d55b8fa.svg"
+  }
+]);
